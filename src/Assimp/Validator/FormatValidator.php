e68dba50a6aa69f921786894df1ed77ac7c5125b
magdev <magdev3.0@googlemail.com>
2013-08-28 21:08:34 +0200

    added validators

---
+++ src/Assimp/Validator/FormatValidator.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Marco Graetsch <magdev3.0@googlemail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author    magdev
+ * @copyright 2013 Marco Graetsch <magdev3.0@googlemail.com>
+ * @package
+ * @license   http://opensource.org/licenses/MIT MIT License
+ */
+
+
+namespace Assimp\Validator;
+
+use Assimp\Command\Verbs\KnowExtensionVerb;
+use Assimp\Command\CommandExecutor;
+
+
+/**
+ * Validate if a format is known by assimp
+ *
+ * @author magdev
+ */
+class FormatValidator
+{
+	/**
+	 * Validate a format string
+	 *
+	 * @param string $format
+	 * @return boolean
+	 */
+	public function validate($format)
+	{
+		$verb = new KnowExtensionVerb();
+		$verb->setFormat($format);
+		
+		$result = CommandExecutor::execute($verb);
+		return $result[0];
+	}
+}
